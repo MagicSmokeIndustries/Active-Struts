179fcb6ca7ca09c15a76c72017ce0d9cf19e3f59
Chris Woerz <erendrake@gmail.com>
2015-04-25 09:58:42 -0600

    deleted CIT-Util

--- KSP-CIT-Util/KSP-CIT-Util/GlobalConst.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CIT_Util
-{
-    public static class GlobalConst
-    {
-        public const ControlTypes GUIWindowLockMask = ControlTypes.CAMERACONTROLS | ControlTypes.MAP | ControlTypes.ACTIONS_ALL | ControlTypes.KSC_FACILITIES | ControlTypes.EVA_INPUT;
-        public const ControlTypes GUIWindowLockMaskEditor = ControlTypes.EDITOR_LOCK | ControlTypes.EDITOR_SOFT_LOCK;
-    }
-}
